fbe9765ecf9b6da6ab6710cf7af6328fbf77e030
Cai Ni <caini@Cais-iMac.fritz.box>
2024-11-30 20:45:35 +1100

    Add CORSMiddleware

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ class HelloWorldResource:
         resp.media = {"message": "Hello, World!"}
         resp.status = falcon.HTTP_200
 
+
+class CORSMiddleware:
+    async def process_response(self, req, resp, resource, req_succeeded):
+        resp.set_header("Access-Control-Allow-Origin", "*")  # Allow all origins
+        resp.set_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")  # Allowed methods
+        resp.set_header("Access-Control-Allow-Headers", "Content-Type")  # Allowed headers
+
 # Create Falcon app and add a route
-app = falcon.asgi.App()
+app = falcon.asgi.App(middleware=[CORSMiddleware()])
 app.add_route('/hello', HelloWorldResource())
