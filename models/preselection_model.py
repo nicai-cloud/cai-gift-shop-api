a5373dcbe421624a68c79a12244f25a0d930a1e2
Cai Ni <caini@Cais-iMac.fritz.box>
2024-12-12 06:52:24 +1100

    Add Index to preselection.name column

--- models/preselection_model.py
+++ models/preselection_model.py
@@ -1,16 +1,18 @@
 from datetime import datetime
 from sqlalchemy import Column, DateTime, Float, ForeignKey, Integer, String
 from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy_serializer import SerializerMixin
+
 
 from models.base import Base
 
 
-class PreselectionModel(Base):
+class PreselectionModel(Base, SerializerMixin):
     __tablename__ = "preselection"
 
     id = Column(Integer, primary_key=True, unique=True, nullable=False)
     image_src = Column(String, nullable=False)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, index=True)
     description = Column(String, nullable=False)
     price = Column(Float, nullable=False)
 
