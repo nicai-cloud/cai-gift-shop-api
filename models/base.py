48ba20bb2c5eea9560ed8835ddbea85d6984ac65
Cai <nicai.msn@gmail.com>
2024-12-12 14:12:45 +1100

    Turn model classes from snake case into camel case when serialised to python objects

--- models/base.py
+++ models/base.py
@@ -7,9 +7,28 @@ class NotDeletedQuery(Query):
         self.filter_by(deleted_at=None)
 
 
+def to_camel_case(snake_str):
+    """Convert snake_case to camelCase."""
+    components = snake_str.split('_')
+    return components[0] + ''.join(component.title() for component in components[1:])
+
+
 class BaseModel:
     deleted_at = Column(DateTime, nullable=True)
     query_class = NotDeletedQuery
+
+    def to_dict(self):
+        """
+        Convert the SQLAlchemy object into a dictionary
+        with camelCase keys.
+        """
+        result = {}
+        for column in self.__table__.columns:
+            snake_key = column.name
+            camel_key = to_camel_case(snake_key)
+            result[camel_key] = getattr(self, snake_key)
+        return result
+
 
 metadata = MetaData()
 Base = declarative_base(metadata=metadata, cls=BaseModel)
