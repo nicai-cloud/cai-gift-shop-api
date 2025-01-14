83c3f4a8bd6f3ea0f809637010e92c56cfaa317b
Cai <nicai.msn@gmail.com>
2025-01-15 08:16:34 +1100

    Fix coupon

--- api/coupon.py
+++ api/coupon.py
@@ -18,6 +18,7 @@ class CouponRequestHandler(RequestHandler):
 
     @route.get("/{code}", auth_exempt=True)
     async def get_coupon_by_code(self, req, resp, code):
+        code = req.params.get('code')
         coupon = await self.coupon_feature.get_coupon_by_code(code)
         if coupon is None:
             raise HTTPNotFound(description="Coupon not found")
