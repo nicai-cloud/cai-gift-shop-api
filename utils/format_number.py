c142da482a767f385d0f08a9159afafa1359d2f3
Cai Ni <caini@Cais-iMac.fritz.box>
2025-01-27 18:49:30 +1100

    Format price from 1000 to 1,000

--- utils/format_number.py
+++ utils/format_number.py
@@ -1,4 +1,5 @@
 from decimal import Decimal, ROUND_HALF_UP
 
-def format_number(obj: Decimal) -> str:
-    return str(obj.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP))
+def format_number(price: Decimal) -> str:
+    price_with_decimal = price.quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
+    return f"{price_with_decimal:,.2f}"
