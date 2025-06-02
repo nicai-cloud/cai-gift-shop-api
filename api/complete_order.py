e24c39e58a630e32284b3b0e6a127a62586cff51
Cai <nicai.msn@gmail.com>
2025-06-02 15:40:27 +1000

    Temporarily disable checking and update of stocks

--- api/complete_order.py
+++ api/complete_order.py
@@ -87,11 +87,12 @@ class CompleteOrderRequestHandler(RequestHandler):
                     description="The provided coupon_code is invalid."
                 )
 
+        # TODO: re-enable stocks checking when feasible
         # Check if there are enough stocks
-        bag_quantities, item_quantities = await self.order_feature.calculate_order_quantities(order_items)
-        stocks_available = await self.inventory_feature.check_stock_availability(bag_quantities, item_quantities)
-        if not stocks_available:
-            raise HTTPError(status="400", description="Out of stock")
+        # bag_quantities, item_quantities = await self.order_feature.calculate_order_quantities(order_items)
+        # stocks_available = await self.inventory_feature.check_stock_availability(bag_quantities, item_quantities)
+        # if not stocks_available:
+        #     raise HTTPError(status="400", description="Out of stock")
         
         # Calculate subtotal and make the payment
         subtotal, subtotal_after_discount = await self.order_feature.calculate_subtotal(order_items, coupon.discount_percentage if coupon else 0)
@@ -100,8 +101,9 @@ class CompleteOrderRequestHandler(RequestHandler):
         order_total = round(subtotal_after_discount + shipping_cost, 2)
         await self.payment_method_feature.create_payment_intent(payment_method_id, order_total)
 
+        # TODO: re-enable stocks checking when feasible
         # If the code reaches here, it means the payment is successful, then we update inventories
-        await self.inventory_feature.update_inventories(bag_quantities, item_quantities)
+        # await self.inventory_feature.update_inventories(bag_quantities, item_quantities)
         
         # Create customer
         customer_id = await self.customer_feature.create_customer(first_name, last_name, email, mobile)
